6285c43c59ce85b8a0235117bf979afbf53abab6
Matthias Bogarin <mabcsoftwaredev@gmail.com>
2022-07-02 11:20:09 -0400

    finished some extra packages and editing on the new estimate modal

--- bogarin_tools/estimate_creator/views.py
+++ bogarin_tools/estimate_creator/views.py
@@ -6,10 +6,20 @@ from django.forms.models import model_to_dict
 import json
 from .models import *
 from datetime import datetime
-import distutils
 import distutils.util
-import csv
-import io
+import os, calendar, requests, pdfkit, base64, uuid
+
+pdfOptions = {
+    'orientation': 'landscape',
+    'page-size': 'A4',
+    'margin-top': '0.5in',
+    'margin-right': '0.5in',
+    'margin-bottom': '0.5in',
+    'margin-left': '0.5in',
+    'encoding': "UTF-8",
+    'zoom':'.9',
+    'enable-local-file-access': ""
+}
 
 import pprint
 pp = pprint.PrettyPrinter()
@@ -19,3 +29,11 @@ def index(request):
         'all_files': Estimates.objects.all(),
     }
     return render(request, 'estimate_creator/creator.html', context)
+
+def base64EncodeIOFile(ioFilePath:str) -> str:
+    """
+    function to read pdf file and convert binary to base64
+    returns base64 bytes of pdf
+    """
+    with open(ioFilePath, "rb") as pdf_file:
+        return base64.b64encode(pdf_file.read()).decode('utf-8')
---
+++ bogarin_tools/static/javascript/creator.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+    console.log("Hello Creator Js has been loaded in");
+});
